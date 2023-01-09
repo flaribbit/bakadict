c738500442c6866df26fe45744cf6359a8a603ed
梦飞翔 <1149761294@qq.com>
2023-01-09 02:36:03 +0000

    使用yansi上色

--- src/types.rs
+++ src/types.rs
@@ -2,6 +2,7 @@ pub struct WordItem {
     pub word: String,
     pub pronounce: String,
     pub accent: String,
+    pub type_: String,
     pub explain: String,
     pub sentences: String,
 }
@@ -10,10 +11,16 @@ use std::fmt;
 
 impl fmt::Display for WordItem {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use yansi::Paint;
         write!(
             f,
-            "{}\n{} {}\n{}\n{}",
-            self.word, self.pronounce, self.accent, self.explain, self.sentences
+            "{}\n{} {}\n{} {}\n{}",
+            Paint::new(&self.word).bold(),
+            Paint::red(&self.pronounce),
+            Paint::red(&self.accent),
+            Paint::blue("【".to_owned() + &self.type_ + "】"),
+            self.explain,
+            Paint::new(&self.sentences).dimmed(),
         )
     }
 }
@@ -21,11 +28,12 @@ impl fmt::Display for WordItem {
 #[test]
 fn test_display_word_item() {
     let word_item = WordItem {
-        word: "".to_owned(),
-        pronounce: "".to_owned(),
-        accent: "".to_owned(),
-        explain: "".to_owned(),
-        sentences: "".to_owned(),
+        word: "蛙".to_owned(),
+        pronounce: "かえる".to_owned(),
+        accent: "◎".to_owned(),
+        type_: "名词".to_owned(),
+        explain: "青蛙。".to_owned(),
+        sentences: "彼には何を言っても蛙の面に水だ。\n跟他说什么也不顶用。".to_owned(),
     };
     println!("{word_item}")
 }
